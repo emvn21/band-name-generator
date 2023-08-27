4f10c62fb0134e720583db88262ece272fa2c434
Emmanuel van Berlo <90194491+emvn21@users.noreply.github.com>
2023-08-27 13:40:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# this prints the welcome message
+print("Welcome to the Band Name Generator.")
+
+#this prompts the input message for the street
+street = input("What's the name of the city you grew up in?\n")
+
+# this prompt the input message for the pet
+pet = input("What's your pet's name?\n")
+
+# this prints out the result by adding the street and pet names
+print("Your band name could be " + street + " " + pet)
